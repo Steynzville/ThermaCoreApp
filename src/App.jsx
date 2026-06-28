5851e00f858cf6f4d14c6f405ed9eb7b24b3b011
Steynzville <steyn.enslin@gmail.com>
2026-06-28 20:43:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,9 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if (pathname) {
+      window.scrollTo(0, 0);
+    }
   }, [pathname]);
 
   return null;
